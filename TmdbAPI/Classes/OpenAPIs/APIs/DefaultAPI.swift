ff6234b1eaa8075dd8ed0add4d713023df47e27b
vegiwoo <vegiwoo@protonmail.com>
2021-03-05 15:12:03 +0300

    for release v0.0.2

--- TmdbAPI/Classes/OpenAPIs/APIs/DefaultAPI.swift
+++ TmdbAPI/Classes/OpenAPIs/APIs/DefaultAPI.swift
@@ -9,6 +9,51 @@ import Foundation
 
 open class DefaultAPI {
     /**
+     Get the list of official genres for movies.
+     
+     - parameter apiKey: (query) API key for using the service. 
+     - parameter apiResponseQueue: The queue on which api response is dispatched.
+     - parameter completion: completion handler to receive the data and the error objects
+     */
+    open class func genreMovieListGet(apiKey: String, apiResponseQueue: DispatchQueue = TmdbAPIAPI.apiResponseQueue, completion: @escaping ((_ data: InlineResponse2001?, _ error: Error?) -> Void)) {
+        genreMovieListGetWithRequestBuilder(apiKey: apiKey).execute(apiResponseQueue) { result -> Void in
+            switch result {
+            case let .success(response):
+                completion(response.body, nil)
+            case let .failure(error):
+                completion(nil, error)
+            }
+        }
+    }
+
+    /**
+     Get the list of official genres for movies.
+     - GET /genre/movie/list
+     - parameter apiKey: (query) API key for using the service. 
+     - returns: RequestBuilder<InlineResponse2001> 
+     */
+    open class func genreMovieListGetWithRequestBuilder(apiKey: String) -> RequestBuilder<InlineResponse2001> {
+        let path = "/genre/movie/list"
+        let URLString = TmdbAPIAPI.basePath + path
+        let parameters: [String: Any]? = nil
+
+        var url = URLComponents(string: URLString)
+        url?.queryItems = APIHelper.mapValuesToQueryItems([
+            "api_key": apiKey.encodeToJSON(),
+        ])
+
+        let nillableHeaders: [String: Any?] = [
+            :
+        ]
+
+        let headerParameters = APIHelper.rejectNilHeaders(nillableHeaders)
+
+        let requestBuilder: RequestBuilder<InlineResponse2001>.Type = TmdbAPIAPI.requestBuilderFactory.getBuilder()
+
+        return requestBuilder.init(method: "GET", URLString: (url?.string ?? URLString), parameters: parameters, headers: headerParameters)
+    }
+
+    /**
      Get a list of the current popular movies on TMDb. This list updates daily.
      
      - parameter apiKey: (query) API key for using the service. 
--- docs/DefaultAPI.md
+++ docs/DefaultAPI.md
@@ -4,8 +4,57 @@ All URIs are relative to *https://api.themoviedb.org/3*
 
 Method | HTTP request | Description
 ------------- | ------------- | -------------
+[**genreMovieListGet**](DefaultAPI.md#genremovielistget) | **GET** /genre/movie/list | Get the list of official genres for movies.
 [**moviePopularGet**](DefaultAPI.md#moviepopularget) | **GET** /movie/popular | Get a list of the current popular movies on TMDb. This list updates daily.
 
+
+# **genreMovieListGet**
+```swift
+    open class func genreMovieListGet(apiKey: String, completion: @escaping (_ data: InlineResponse2001?, _ error: Error?) -> Void)
+```
+
+Get the list of official genres for movies.
+
+### Example 
+```swift
+// The following code samples are still beta. For any issue, please report via http://github.com/OpenAPITools/openapi-generator/issues/new
+import TmdbAPI
+
+let apiKey = "apiKey_example" // String | API key for using the service.
+
+// Get the list of official genres for movies.
+DefaultAPI.genreMovieListGet(apiKey: apiKey) { (response, error) in
+    guard error == nil else {
+        print(error)
+        return
+    }
+
+    if (response) {
+        dump(response)
+    }
+}
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **apiKey** | **String** | API key for using the service. | 
+
+### Return type
+
+[**InlineResponse2001**](InlineResponse2001.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
 
 # **moviePopularGet**
 ```swift
