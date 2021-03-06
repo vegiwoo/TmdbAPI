04046e3376b4e1f1c1ec720603028a1244cf93ad
vegiwoo <vegiwoo@protonmail.com>
2021-03-06 14:36:56 +0300

    for release v0.0.11

--- TmdbAPI/Classes/OpenAPIs/APIs/DefaultAPI.swift
+++ TmdbAPI/Classes/OpenAPIs/APIs/DefaultAPI.swift
@@ -54,6 +54,56 @@ open class DefaultAPI {
     }
 
     /**
+     Get the primary information about a movie.
+     
+     - parameter movieId: (path) The movie ID 
+     - parameter apiKey: (query) API key for using the service. 
+     - parameter apiResponseQueue: The queue on which api response is dispatched.
+     - parameter completion: completion handler to receive the data and the error objects
+     */
+    open class func movieMovieIdGet(movieId: Int, apiKey: String, apiResponseQueue: DispatchQueue = TmdbAPIAPI.apiResponseQueue, completion: @escaping ((_ data: InlineResponse2002?, _ error: Error?) -> Void)) {
+        movieMovieIdGetWithRequestBuilder(movieId: movieId, apiKey: apiKey).execute(apiResponseQueue) { result -> Void in
+            switch result {
+            case let .success(response):
+                completion(response.body, nil)
+            case let .failure(error):
+                completion(nil, error)
+            }
+        }
+    }
+
+    /**
+     Get the primary information about a movie.
+     - GET /movie/{movie_id}
+     - parameter movieId: (path) The movie ID 
+     - parameter apiKey: (query) API key for using the service. 
+     - returns: RequestBuilder<InlineResponse2002> 
+     */
+    open class func movieMovieIdGetWithRequestBuilder(movieId: Int, apiKey: String) -> RequestBuilder<InlineResponse2002> {
+        var path = "/movie/{movie_id}"
+        let movieIdPreEscape = "\(APIHelper.mapValueToPathItem(movieId))"
+        let movieIdPostEscape = movieIdPreEscape.addingPercentEncoding(withAllowedCharacters: .urlPathAllowed) ?? ""
+        path = path.replacingOccurrences(of: "{movie_id}", with: movieIdPostEscape, options: .literal, range: nil)
+        let URLString = TmdbAPIAPI.basePath + path
+        let parameters: [String: Any]? = nil
+
+        var url = URLComponents(string: URLString)
+        url?.queryItems = APIHelper.mapValuesToQueryItems([
+            "api_key": apiKey.encodeToJSON(),
+        ])
+
+        let nillableHeaders: [String: Any?] = [
+            :
+        ]
+
+        let headerParameters = APIHelper.rejectNilHeaders(nillableHeaders)
+
+        let requestBuilder: RequestBuilder<InlineResponse2002>.Type = TmdbAPIAPI.requestBuilderFactory.getBuilder()
+
+        return requestBuilder.init(method: "GET", URLString: (url?.string ?? URLString), parameters: parameters, headers: headerParameters)
+    }
+
+    /**
      Get a list of the current popular movies on TMDb. This list updates daily.
      
      - parameter apiKey: (query) API key for using the service. 
--- docs/DefaultAPI.md
+++ docs/DefaultAPI.md
@@ -5,6 +5,7 @@ All URIs are relative to *https://api.themoviedb.org/3*
 Method | HTTP request | Description
 ------------- | ------------- | -------------
 [**genreMovieListGet**](DefaultAPI.md#genremovielistget) | **GET** /genre/movie/list | Get the list of official genres for movies.
+[**movieMovieIdGet**](DefaultAPI.md#moviemovieidget) | **GET** /movie/{movie_id} | Get the primary information about a movie.
 [**moviePopularGet**](DefaultAPI.md#moviepopularget) | **GET** /movie/popular | Get a list of the current popular movies on TMDb. This list updates daily.
 
 
@@ -44,6 +45,56 @@ Name | Type | Description  | Notes
 ### Return type
 
 [**InlineResponse2001**](InlineResponse2001.md)
+
+### Authorization
+
+No authorization required
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
+ - **Accept**: application/json
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **movieMovieIdGet**
+```swift
+    open class func movieMovieIdGet(movieId: Int, apiKey: String, completion: @escaping (_ data: InlineResponse2002?, _ error: Error?) -> Void)
+```
+
+Get the primary information about a movie.
+
+### Example 
+```swift
+// The following code samples are still beta. For any issue, please report via http://github.com/OpenAPITools/openapi-generator/issues/new
+import TmdbAPI
+
+let movieId = 987 // Int | The movie ID
+let apiKey = "apiKey_example" // String | API key for using the service.
+
+// Get the primary information about a movie.
+DefaultAPI.movieMovieIdGet(movieId: movieId, apiKey: apiKey) { (response, error) in
+    guard error == nil else {
+        print(error)
+        return
+    }
+
+    if (response) {
+        dump(response)
+    }
+}
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **movieId** | **Int** | The movie ID | 
+ **apiKey** | **String** | API key for using the service. | 
+
+### Return type
+
+[**InlineResponse2002**](InlineResponse2002.md)
 
 ### Authorization
 
