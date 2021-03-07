634288a768148c0cebe4d46ffc4466317f65df5f
vegiwoo <vegiwoo@protonmail.com>
2021-03-07 14:27:30 +0300

    for release v0.0.15

--- TmdbAPI/Classes/OpenAPIs/Models/Collection.swift
+++ TmdbAPI/Classes/OpenAPIs/Models/Collection.swift
@@ -11,27 +11,21 @@ public struct Collection: Codable {
 
     public var id: Int?
     public var name: String?
-    public var overview: String?
     public var posterPath: String?
     public var backdropPath: String?
-    public var parts: [MovieListResultObject]?
 
-    public init(id: Int? = nil, name: String? = nil, overview: String? = nil, posterPath: String? = nil, backdropPath: String? = nil, parts: [MovieListResultObject]? = nil) {
+    public init(id: Int? = nil, name: String? = nil, posterPath: String? = nil, backdropPath: String? = nil) {
         self.id = id
         self.name = name
-        self.overview = overview
         self.posterPath = posterPath
         self.backdropPath = backdropPath
-        self.parts = parts
     }
 
     public enum CodingKeys: String, CodingKey, CaseIterable {
         case id
         case name
-        case overview
         case posterPath = "poster_path"
         case backdropPath = "backdrop_path"
-        case parts
     }
 
 }
--- docs/Collection.md
+++ docs/Collection.md
@@ -5,10 +5,8 @@ Name | Type | Description | Notes
 ------------ | ------------- | ------------- | -------------
 **id** | **Int** |  | [optional] 
 **name** | **String** |  | [optional] 
-**overview** | **String** |  | [optional] 
 **posterPath** | **String** |  | [optional] 
 **backdropPath** | **String** |  | [optional] 
-**parts** | [MovieListResultObject] |  | [optional] 
 
 [[Back to Model list]](../README.md#documentation-for-models) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to README]](../README.md)
 
